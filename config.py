f03b14b4dfa50b1949d8747e6e7b862d85968b61
BtwToxic <btwalexxd@gmail.com>
2025-12-30 21:12:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = 21705136
+API_HASH = "78730e89d196e160b0f1992018c6cb19"
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+MONGO_DB_URI = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
 
-OWNER_ID = int(getenv("OWNER_ID", 7250012103))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BRANDED_WORLD")
+OWNER_ID = 6944519938
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/techbotss")
