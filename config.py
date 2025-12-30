cc78794298352c051c091375aad96f7c82a480e0
BtwToxic <btwalexxd@gmail.com>
2025-12-30 21:13:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ load_dotenv()
 API_ID = 21705136
 API_HASH = "78730e89d196e160b0f1992018c6cb19"
 
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = "8384712402:AAF7wA4Jj8Umk_js2u4tHvsV6puOZ-BWdSA"
 MONGO_DB_URI = "mongodb+srv://Krishna:pss968048@cluster0.4rfuzro.mongodb.net/?retryWrites=true&w=majority"
 
 OWNER_ID = 6944519938
